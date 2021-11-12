84954270edda91d1d436098698e129a4794bdd51
Vinicius-Mance <vinimanceprof@gmail.com>
2021-11-11 21:47:26 -0300

    correções e limpeza de dados

--- src/main/java/br/com/control/Controller.java
+++ src/main/java/br/com/control/Controller.java
@@ -2,9 +2,14 @@ package br.com.control;
 
 import br.com.beans.Usuario;
 import br.com.repository.UsuarioRepositorio;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 
 @RestController
@@ -22,22 +27,14 @@ public class Controller {
         return acoes.findAll();
     }
 
-//    // Logout
-//    @RequestMapping(value = "/logout", method = RequestMethod.GET)
-//    public @ResponseBody
-//    void logout() {
-//
-//    }
-
     // Apagar usuário por id
-    @Cacheable
     @RequestMapping(value = "/apagar/{id}", method = RequestMethod.GET)
     public @ResponseBody void apagarUsuario(@PathVariable int id){
         acoes.delete(acoes.findById(id));
     }
 
 
-    // Cadastrar usuarios
+    // Cadastrar/alterar usuarios
     @RequestMapping(value = "/salvar", method = RequestMethod.POST)
     public @ResponseBody
     Usuario cadastrar(@RequestBody Usuario usuario) {
@@ -45,11 +42,10 @@ public class Controller {
     }
 
     // Pegar usuario por id
+    @Cacheable
     @RequestMapping(value = "/listar/{id}", method = RequestMethod.GET)
     public @ResponseBody Usuario listarUsuario(@PathVariable int id){
         return acoes.findById(id);
     }
-
-
 
 }
